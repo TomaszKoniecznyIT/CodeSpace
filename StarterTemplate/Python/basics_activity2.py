888aff3b0e8a83b333ff1eae854d419c26e18f46
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-04 13:32:59 +0000

    performing division and save as resultperforming division and save as result

--- StarterTemplate/Python/basics_activity2.py
+++ StarterTemplate/Python/basics_activity2.py
@@ -1,2 +1,9 @@
+# Receiving two numbers from the user, conert to float and storing them in variables
 number1 = float(input("Input the first number: "))
 number2 = float(input("Input the second number: "))
+
+# performing division and save as result
+result = number1/number2
+
+# printing the result
+print(result)
