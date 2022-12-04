57963bfa5c0d1267281e1885055b9d2fdf2a08f3
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-04 13:16:19 +0000

    example mathematical operattions

---
+++ StarterTemplate/Python/basics_activity1.py
@@ -0,0 +1,18 @@
+# Execution in print
+print(-5+8*6)
+print((55+9)%9)
+print(20+-3*5/8)
+print(5+15/3*2-8%3)
+
+# separated by a dashed line
+print("---"*5)
+
+# Saving in a variable and then printing the value of the variable
+a=-5+8*6
+b=(55+9)%9
+c=20+-3*5/8
+d=5+15/3*2-8%3
+print(a)
+print(b)
+print(c)
+print(d)
