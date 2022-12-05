0efe87dc5d67b22dac7e88df1fd18bf4547ccb74
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-05 20:58:05 +0000

    three numbers equal, different and ...

---
+++ StarterTemplate/Python/control_statements_activity1.py
@@ -0,0 +1,8 @@
+# Receiving two numbers from the user, conert to float and storing them in variables
+number1 = float(input("Input the first number: "))
+number2 = float(input("Input the second number: "))
+number3 = float(input("Input the third number: "))
+
+if number1 == number2 and number1 == number3:
+    print("All numbers are equal")
+elif number1 
