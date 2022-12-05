92c5fcfcf7c9b06ff334a67f28e8b64cc227b8ca
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-05 21:09:52 +0000

    adding commentsadding comments

--- StarterTemplate/Python/control_statements_activity1.py
+++ StarterTemplate/Python/control_statements_activity1.py
@@ -3,9 +3,12 @@ number1 = float(input("Input the first number: "))
 number2 = float(input("Input the second number: "))
 number3 = float(input("Input the third number: "))
 
+# check if they are all equal
 if number1 == number2 and number1 == number3:
     print("All numbers are equal")
+# check if they are all different
 elif number1 != number2 and number2 != number3 and number3 != number1:
     print("All numbers are different")
+# if they are neither equal nor different
 else:
     print("Neither all are equal or different")
