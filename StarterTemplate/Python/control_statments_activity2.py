6a8611072773febd0f83c0992c5bd653855369fe
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-07 09:57:29 +0000

    adding a comparison of numbers

--- StarterTemplate/Python/control_statments_activity2.py
+++ StarterTemplate/Python/control_statments_activity2.py
@@ -2,3 +2,12 @@
 number1 = float(input("Input the first number: "))
 number2 = float(input("Input the second number: "))
 number3 = float(input("Input the third number: "))
+
+if number1 >= number2 and number2 >= number3 and number1 != number3:
+    print("Numbers in descending order")
+elif number1 <= number2 and number2 <= number3 and number1 != number3:
+    print("Numbers in increasing order")
+elif number1 == number2 and number2 == number3:
+    print("all numbers are equal")
+else:
+    print("Neither increasing or decreasing order")
