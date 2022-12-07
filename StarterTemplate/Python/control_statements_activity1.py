2f867c0286b7980679e692d7edc6c16d311709fc
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-07 09:44:18 +0000

    Receiving 3 numbers from the user

--- StarterTemplate/Python/control_statements_activity1.py
+++ StarterTemplate/Python/control_statements_activity1.py
@@ -1,4 +1,4 @@
-# Receiving two numbers from the user, conert to float and storing them in variables
+# Receiving three numbers from the user, conert to float and storing them in variables
 number1 = float(input("Input the first number: "))
 number2 = float(input("Input the second number: "))
 number3 = float(input("Input the third number: "))
---
+++ StarterTemplate/Python/control_statments_activity2.py
@@ -0,0 +1,4 @@
+# Receiving 3 numbers from the user, conert to float and storing them in variables
+number1 = float(input("Input the first number: "))
+number2 = float(input("Input the second number: "))
+number3 = float(input("Input the third number: "))
