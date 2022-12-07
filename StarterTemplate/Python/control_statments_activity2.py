bbfdb2e1d6b8e36f7fb3aa1d7a153557d2fcc2e8
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-07 10:08:28 +0000

    adding comments

--- StarterTemplate/Python/control_statments_activity2.py
+++ StarterTemplate/Python/control_statments_activity2.py
@@ -3,10 +3,15 @@ number1 = float(input("Input the first number: "))
 number2 = float(input("Input the second number: "))
 number3 = float(input("Input the third number: "))
 
+
+# checking whether they are decreasing (the last condition excludes the situation when all numbers are equal)
 if number1 >= number2 and number2 >= number3 and number1 != number3:
     print("Numbers in descending order")
+
+# checking whether they are  (the last condition excludes the situation when all numbers are equal)    
 elif number1 <= number2 and number2 <= number3 and number1 != number3:
     print("Numbers in increasing order")
+#   check if they are equal
 elif number1 == number2 and number2 == number3:
     print("all numbers are equal")
 else:
