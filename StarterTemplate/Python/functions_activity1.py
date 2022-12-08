8e962696a5f30d3dab619df3db38f92a67cc784c
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-08 20:27:09 +0000

    add comments

--- StarterTemplate/Python/functions_activity1.py
+++ StarterTemplate/Python/functions_activity1.py
@@ -1,6 +1,8 @@
+# the function displays the list
 def list_content (list1):
     print(f"The content of the list is: {list1}")
 
+# function displays the maximum element of the list
 def max_list (list1):
     print(f"The max value in the list: {max(list1)}")
 
