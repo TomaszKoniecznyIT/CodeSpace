6de9649a19864f2ce59bfc6abc20c9e4661479c9
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-08 20:24:12 +0000

    functions list content and max

---
+++ StarterTemplate/Python/functions_activity1.py
@@ -0,0 +1,10 @@
+def list_content (list1):
+    print(f"The content of the list is: {list1}")
+
+def max_list (list1):
+    print(f"The max value in the list: {max(list1)}")
+
+
+list1 = [10, 2, 3, 4,7]
+list_content(list1)
+max_list(list1)
