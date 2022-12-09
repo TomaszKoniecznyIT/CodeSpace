18cbdbd1db18b4e781c59e190671a27eaa8c112e
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-09 13:33:54 +0000

    function prime number check and add comment

--- StarterTemplate/Python/functions_activity3.py
+++ StarterTemplate/Python/functions_activity3.py
@@ -1,11 +1,12 @@
+# function checks whether a given number is a prime number
 def prime_number(number):
-    prime_num = True
+    # If the given number is not greater than 1 (contrary to the definition), we display a message
     if number <= 1:
         return "the definition says that numbers greater than one..."
+    
     else:
-        for i in range(2,number):
-            if number % i == 0:
-                prime_num = False
+        for i in range(2,number):        # in the range from 2 to the number given - 1
+            if number % i == 0:          # we check whether it divides completely
                 return f"The number {number} is not a prime number."
     
     return f"The number {number} is a prime number."
