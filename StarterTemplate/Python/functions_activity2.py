f231f171f5c381223695d002c117a5da125a4dbb
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-09 12:43:33 +0000

    add comments

--- StarterTemplate/Python/functions_activity2.py
+++ StarterTemplate/Python/functions_activity2.py
@@ -1,8 +1,12 @@
+# factorial function
 def factorial(n):
+    # displaying information when the given number is negative
     if n < 0:
         print("The factorial is not defined for negative number")
+    # when the given number is 0
     elif n == 0:
         print("The factorial of 0! is 1 (0!=1)")
+    # for the other cases, calculating the factorial of the number
     else:
         result = 1
         for i in range(1,n+1):
@@ -10,5 +14,7 @@ def factorial(n):
         print(f"The factorial of {n}! is {result}. {n}! = {result}")
 
 
+# receiving a number from the user
 n = int(input("Enter the number for which you want to calculate the factorial: "))
+# function call
 factorial(n)
