d79adf9912c1da502c563b51d06c7e64324ceaf5
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-11 13:40:49 +0000

    adding comments

--- StarterTemplate/Python/challenge.py
+++ StarterTemplate/Python/challenge.py
@@ -1,8 +1,7 @@
 from random import choice   # import from random choice function
 
+
 # a function that receives the player's choice
-
-
 def user_selection():
     while True:     # looping when the user enters a different letter like R,r,P,p,S,s
         user = input("Enter a choice (Rock(r), Paper(p), Scissores(s)): ")
@@ -28,38 +27,48 @@ def display_rules():
     print("SCISSORS cut PAPER")
 
 
+# function asking whether to continue playing
 def if_play_again():
-    while True:
+    while True:    # looping when the user enters a different letter like Y,y,N,n
         play_a = input("Play again? (y/n): ")
-        play_a = play_a.lower()
-        if play_a == "y":
+        play_a = play_a.lower()   # converting a capital letter to a lowercase letter
+        if play_a == "y":  # return true if y
             return True
         elif play_a == "n":
-            return False
+            return False   # return false if n
 
 
+# a dictionary that has r,s,p as the key and full words as the values
 dict_full_name = {"r": "ROCK", "p": "PAPER", "s": "SCISSORS"}
 
-display_rules()
+display_rules()  # rules display
+# set to 0 computer and player points
 score_player = 0
 score_computer = 0
-play_again = True
+play_again = True  # setting replay to true
 
+# looping the game until the play_again variable becomes false
 while play_again:
+    # running a random function for the computer and assigning it to the variable computer
     computer = computer_selection()
+    # calling the function receiving from the player of choice and writing to the variable player
     player = user_selection()
+    # commands for the same choice
     if computer == player:
+        # (dict_full_name[computer]) - referencing a dictionary and replacing e.g. r with ROCK
         print(f"Both players selected {dict_full_name[computer]}. It's a tie!")
+    # commands when the computer wins
     elif (computer == "r" and player == "s") or (computer == "p" and player == "r") or (computer == "s" and player == "p"):
         print(
             f"The computer chose {dict_full_name[computer]}, You chose {dict_full_name[player]}.")
         print("COMPUTER WINS !!!")
-        score_computer += 1
+        score_computer += 1   # adding a point for the computer
+    # commands when the computer loses
     else:
         print(
             f"The computer chose {dict_full_name[computer]}, You chose {dict_full_name[player]}.")
         print("YOU WINS !!!")
-        score_player += 1
+        score_player += 1  # adding a player point
     play_again = if_play_again()
 
 print("FINAL SCORES:")
