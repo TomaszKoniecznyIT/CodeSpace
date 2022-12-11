3df8f77326defc57c389bf275adfce6a32da7454
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2022-12-11 12:12:10 +0000

    function that receives the computer choice

--- StarterTemplate/Python/challenge.py
+++ StarterTemplate/Python/challenge.py
@@ -1,7 +1,19 @@
+from random import choice
+
+# a function that receives the player's choice
 def user_selection():
-    while True:
+    while True:     # looping when the user enters a different letter like R,r,P,p,S,s
         user = input("Enter a choice (Rock(r), Paper(p), Scissores(s)): ")
-        user= user.lower()
+        user= user.lower()    # converting a capital letter to a lowercase letter
         if user == "r" or user == "p" or user == "s":
             return user
+
+
+def computer_selection():
+    letters_to_choose = ["r", "p", "s"]
+    computer = choice(letters_to_choose)
+    return computer
+
+
+
 
