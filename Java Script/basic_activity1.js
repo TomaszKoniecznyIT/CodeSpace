4de18e0bb5d2cc6f20a8cac46cedfcb140643d13
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-04 22:37:45 +0000

    JS basics activity 1

---
+++ Java Script/basic_activity1.js
@@ -0,0 +1,11 @@
+// writing to variables a,b,c,d the values of given actions
+const a = -5 + 8 * 6;
+const b = (55 + 9) % 9;
+const c = 20 + -3 * 5 / 8;
+const d = 5 + 15 / 3 * 2 - 8 % 3
+
+// displaying the results of these actions on the console (variable values)
+console.log(`Test resukt a: ${a}. `);
+console.log(`Test resukt b: ${b}. `);
+console.log(`Test resukt c: ${c}. `);
+console.log(`Test resukt d: ${d}. `);
