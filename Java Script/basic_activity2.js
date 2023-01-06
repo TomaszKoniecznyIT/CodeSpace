e48f74febe3db3cbd6eb10545d56001efd22d5e9
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-06 21:07:55 +0000

    basic activity 2

---
+++ Java Script/basic_activity2.js
@@ -0,0 +1,6 @@
+// receiving two numbers from the user and storing them in variables
+first_number = Number(prompt ("enter the first number: "))
+second_number = Number(prompt ("enter the second number: "))
+
+// displaying the result
+console.log(`The division of ${first_number} and the ${second_number} is: ${second_number !== 0 ? first_number/second_number : "cannot be divided by 0 !!!" }.`)
