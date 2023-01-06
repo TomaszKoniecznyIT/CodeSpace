5fc5706ac6d2c0b61dbadeee8ee23e9642292fec
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-06 21:11:10 +0000

    add comment

--- Java Script/basic_activity2.js
+++ Java Script/basic_activity2.js
@@ -3,4 +3,5 @@ first_number = Number(prompt ("enter the first number: "))
 second_number = Number(prompt ("enter the second number: "))
 
 // displaying the result
+// in the case of division by 0, instead of the result, the display of the message cannot be divided by 0
 console.log(`The division of ${first_number} and the ${second_number} is: ${second_number !== 0 ? first_number/second_number : "cannot be divided by 0 !!!" }.`)
