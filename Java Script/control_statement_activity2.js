d169f1d8711bfe328d7bff0857d3c131e08ad8b1
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-07 20:48:49 +0000

    comments

--- Java Script/control_statement_activity2.js
+++ Java Script/control_statement_activity2.js
@@ -1,7 +1,9 @@
+// receiving three numbers from the user and storing them as numbers in variables
 first_number = Number(prompt ("enter the first number: "));
 second_number = Number(prompt ("enter the second number: "));
 third_number = Number(prompt ("enter the third number: "));
 
+// check whether they are increasing or decreasing
 if (first_number <= second_number && second_number <= third_number && first_number !== third_number){
     console.log("Increasing order")
 } else if (first_number >= second_number && second_number >= third_number && first_number !== third_number){
