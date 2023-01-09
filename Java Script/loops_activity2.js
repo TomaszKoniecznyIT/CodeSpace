fdc8f05f8c86d3a33e45a53b081f37a8ea28c1a7
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-09 18:35:52 +0000

    Loop activity 2 comment

--- Java Script/loops_activity2.js
+++ Java Script/loops_activity2.js
@@ -1,3 +1,5 @@
+// loop within loop for k from 1 to 10 and j from 1 to 10
+// display k x j = the result of multiplying k times j
 for (let k = 1; k < 11; k++) {
     for (let j = 1; j < 11; j++) {
         console.log(`${k} x ${j} = ${k * j}`)
