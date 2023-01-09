89b18b6180a1538cf01339d92315ed6a4fc3c6a7
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-09 18:25:04 +0000

    Loop activity 1 comment

--- Java Script/loops_activity1.js
+++ Java Script/loops_activity1.js
@@ -1,3 +1,5 @@
+// loop from 0 to 15
+// we display the current value i and depending on whether i is divisible by 2 without remainder odd or even.
 for (let i=0; i < 16; i++){
     console.log(`${i} is ${(i % 2 !== 0) ? "odd" :"even"}`)
 }
