36e6347a9dba1768a5abc345912df881b804d943
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-11 15:12:14 +0000

    function activity 1 comments

--- Java Script/functions_activity1.js
+++ Java Script/functions_activity1.js
@@ -1,7 +1,9 @@
+// a function that displays the content of the array
 function arrayContent(array) {
-  console.log(`The content of array is: ${array}`);
+  console.log(`The content of array is: [${array}]`);
 }
 
+// function that finds the maximum value of an array
 function maxArray(array) {
   let max = array[0];
   for (i = 0; i < array.length; i++) {
@@ -10,6 +12,11 @@ function maxArray(array) {
   return max;
 }
 
+// sample array
 array1 = [10, 2, 3, 4, 7];
+
+// calling the function displaying the contents of the array
 arrayContent(array1);
+
+// displaying information along with calling the function that finds the maximum value of the array
 console.log(`The max value in the array is: ${maxArray(array1)}`);
