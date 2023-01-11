07c5e80a5ce67b523d2105730b63d445ab61cd34
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-11 12:22:04 +0000

    Array activity 1 comments

--- Java Script/arrays_activity1.js
+++ Java Script/arrays_activity1.js
@@ -1,3 +1,5 @@
+// a function that calculates the sum of any array
+// taking successive elements of the array sums up and stores in the sum variable
 function sumArray(array){
     sum = 0;
     for (let i = 0; i < array.length; i++){
@@ -6,5 +8,8 @@ function sumArray(array){
     return sum;
 };
 
+// example array
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// displaying together with calling the function for array a
 console.log(`The sum of array is ${sumArray(a)}.`);
