48b066e707420e84b812f8d28363a76e38b277eb
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-11 13:56:31 +0000

    test

--- Java Script/arrays_activity1.js
+++ Java Script/arrays_activity1.js
@@ -1,12 +1,12 @@
 // a function that calculates the sum of any array
 // taking successive elements of the array sums up and stores in the sum variable
-function sumArray(array){
-    let sum = 0;
-    for (let i = 0; i < array.length; i++){
-        sum += array[i]
-    };
-    return sum;
-};
+function sumArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
 
 // example array
 const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
--- Java Script/arrays_activity2.js
+++ Java Script/arrays_activity2.js
@@ -1,13 +1,13 @@
 // a function that calculates the sum of any array
 // taking successive elements of the array sums up and stores in the sum variable
 // then returns that sum divided by the number of elements in the array
-function averageArray(array){
-    let sum = 0;
-    for (let i = 0; i < array.length; i++){
-        sum += array[i]
-    };
-    return sum / array.length;
-};
+function averageArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
+}
 
 // example array
 const b = [20, 30, 25, 35, -16, 60, -100];
