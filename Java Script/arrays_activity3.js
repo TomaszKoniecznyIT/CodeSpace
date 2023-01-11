99f59e201e52e5c370031f224940969736e24712
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-11 13:02:58 +0000

    Array activity 3 comments

--- Java Script/arrays_activity3.js
+++ Java Script/arrays_activity3.js
@@ -1,3 +1,7 @@
+// function that counts the min and max of the array
+// set the first element of the array to the max and min elements
+// we check if the next element of the array is less than min or greater than max
+// if it is then replace max or min with this value
 function minMaxArray(array){
     let min = array[0];
     let max = array[0];
@@ -9,6 +13,11 @@ function minMaxArray(array){
     console.log(`Minimum value for the above array ${min}`);
 };
 
+// example array
 const c = [25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+
+// displaying the original table
 console.log(`Original array: ${c}`);
+
+// run function for array c
 minMaxArray(c);
