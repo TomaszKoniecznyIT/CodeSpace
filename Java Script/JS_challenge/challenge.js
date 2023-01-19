77900aeb84deb0264425225ae317a4d633e5861b
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-19 18:10:50 +0000

    JS challenge JS btn

--- Java Script/JS_challenge/challenge.js
+++ Java Script/JS_challenge/challenge.js
@@ -1,1 +1,6 @@
-const paperEL = document.querySelector();
+const paperEL = document.querySelector(".btn-paper");
+const rockEL = document.querySelector(".btn-rock");
+const scissorsEL = document.querySelector(".btn-scissors");
+console.log(paperEL);
+console.log(rockEL);
+console.log(scissorsEL);
