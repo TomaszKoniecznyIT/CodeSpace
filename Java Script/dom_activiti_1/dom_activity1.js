73a0696debe358948a9a9791000e750b0f016906
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-19 09:44:20 +0000

    DOM activity 1

---
+++ Java Script/dom_activiti_1/dom_activity1.js
@@ -0,0 +1,8 @@
+const textEl = document.querySelector("#text");
+textEl.classList.add("text");
+
+const btnEl = document.getElementById("jsstyle");
+btnEl.classList.add("btn-set");
+
+text.textContent += " !!!";
+document.querySelector("div").classList.add("div-btn");
