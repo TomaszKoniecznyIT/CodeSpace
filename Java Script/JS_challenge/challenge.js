30b2b1afbfec5be7a7d2144e95a70209c758ad1a
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-19 22:26:02 +0000

    JS challenge comments

--- Java Script/JS_challenge/challenge.js
+++ Java Script/JS_challenge/challenge.js
@@ -1,3 +1,4 @@
+// selecting elements and assigning them to variables
 const paperEL = document.querySelector(".btn-paper");
 const rockEL = document.querySelector(".btn-rock");
 const scissorsEL = document.querySelector(".btn-scissors");
@@ -7,22 +8,29 @@ const winEl = document.querySelector(".win");
 const playerPointEL = document.querySelector("#pla");
 const computerPointEL = document.querySelector("#com");
 
+// declaration of variables
 let userChoice, compChoice, result, playerPoint, computerPoint;
 
+// function to set all items to a new game
 const startGame = () => {
   playerPoint = 0;
   computerPoint = 0;
   winEl.textContent = "START THE GAME";
   chosenEl.textContent = "choose one of the things";
+  // calling the function displaying the points
   displayPoint(playerPoint, computerPoint);
 };
 
+// a function that randomizes the computer's choice
 const drawForComputer = () => {
   const things = ["paper", "rock", "scissors"];
+  //   draw from between (0, 1, 2)
   const drawNumber = Math.trunc(Math.random() * 3);
+  // return the selected element of the array
   return things[drawNumber];
 };
 
+// a function that compares the choices and determines the winners
 const comparisonResult = (player, computer) => {
   if (
     (player === "rock" && computer === "scissors") ||
@@ -39,11 +47,13 @@ const comparisonResult = (player, computer) => {
   } else return "draw";
 };
 
+// the function displays the score
 const displayPoint = (playerPoint, computerPoint) => {
   playerPointEL.textContent = playerPoint;
   computerPointEL.textContent = computerPoint;
 };
 
+// the function displays information about the result
 const display = (result) => {
   if (result === "player") {
     playerPoint++;
@@ -61,12 +71,17 @@ const display = (result) => {
   }
 };
 
+// initial settings
 startGame();
 
+// check if a button has been pressed
 paperEL.addEventListener("click", function () {
   userChoice = "paper";
+  //   calling the random function and saving it to a variable
   compChoice = drawForComputer();
+  // calling the result checking function
   result = comparisonResult(userChoice, compChoice);
+  // calling the function displaying information about the result
   display(result);
 });
 
@@ -85,5 +100,6 @@ scissorsEL.addEventListener("click", function () {
 });
 
 newGameEL.addEventListener("click", function () {
+  // initial settings
   startGame();
 });
