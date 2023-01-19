e3a83eaa21ceefc0276f9f708ff6624a39e5697f
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-19 10:00:03 +0000

    DOM activity 1 add event btn

--- Java Script/dom_activiti_1/dom_activity1.js
+++ Java Script/dom_activiti_1/dom_activity1.js
@@ -1,8 +1,19 @@
+// under textEl writes an element with id text
 const textEl = document.querySelector("#text");
+// to this element we add the text class
 textEl.classList.add("text");
 
+// under btnEl writes an element with id jsstyle
 const btnEl = document.getElementById("jsstyle");
+// to this element we add the btn-set class
 btnEl.classList.add("btn-set");
 
+// to the text in <p> with the id "text" we add !!!
 text.textContent += " !!!";
+
+// to the <div> we add the class div-btn
 document.querySelector("div").classList.add("div-btn");
+
+btnEl.addEventListener("click", function () {
+  btnEl.classList.toggle("pres-btn");
+});
