9044e3dff92063ccb3c37f35d599852b81bac241
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-01-19 14:41:42 +0000

    DOM activity 2

--- Java Script/dom_activity_2/dom_activity2.js
+++ Java Script/dom_activity_2/dom_activity2.js
@@ -1,7 +1,10 @@
-const calculateEl = document.querySelector(".btn");
+const calculateEl = document.querySelector(".btn-rad");
+const resultEl = document.querySelector(".result");
 
 calculateEl.addEventListener("click", function () {
+  const radius = Number(document.querySelector(".input").value);
   if (radius > 0) {
-    console.log(radius);
-  }
+    const result = (4 / 3) * Math.PI * radius ** 3;
+    resultEl.textContent = result;
+  } else resultEl.textContent = "enter a valid value (>0)";
 });
